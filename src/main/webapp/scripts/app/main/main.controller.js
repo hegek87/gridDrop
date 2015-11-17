986b527e19b458eeb99eb1e166da292f2898a2f0
Kenny Hegeland <khegeland@ippon.fr>
2015-11-17 13:32:27 -0500

    Drag and drop works, add some color

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -5,5 +5,26 @@ angular.module('griddropApp')
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+
+            $scope.getRandomContent = function() {
+                return {
+                    score: Math.floor((Math.random() * 100) + 1),
+                    color: $scope.randomColor()
+                }
+            };
+
+            $scope.randomColor = function() {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            };
+
+            $scope.contents = [];
+            for(var i = 0; i < 9; ++i) {
+                $scope.contents.push($scope.getRandomContent());
+            }
         });
     });
